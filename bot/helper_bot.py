4c63c6f451741e2832fc5024d18cf0deb1fee908
sapols <sapols@yahoo.com>
2023-09-16 00:51:29 -0600

    Add support for local vector storage

--- bot/helper_bot.py
+++ bot/helper_bot.py
@@ -1,5 +1,6 @@
 # helper_bot.py
-from config import model_name
+import os
+from config import model_name, deeplake_username
 import deeplake
 from deeplake.util.exceptions import DatasetHandlerError
 from langchain.chat_models import ChatOpenAI
@@ -16,10 +17,10 @@ import tempfile
 EMBEDDINGS = OpenAIEmbeddings(disallowed_special=())
 
 
-def dataset_exists(dataset_path):
+def dataset_exists_online(dataset_name):
     try:
         # Attempt to load the dataset
-        ds = deeplake.load(dataset_path)
+        ds = deeplake.load(f"hub://{deeplake_username}/{dataset_name}")
         ds_existed = True  # If this line is reached, the dataset exists
     except DatasetHandlerError:
         # This exception is raised if the dataset doesn't exist
@@ -27,16 +28,21 @@ def dataset_exists(dataset_path):
     return ds_existed
 
 
-def clone_github_repo(github_url, local_path):
-    try:
-        subprocess.run(['git', 'clone', github_url, local_path], check=True)
+def dataset_exists_locally(dataset_name):
+    if os.path.isdir(f"vector_store/{dataset_name}"):
         return True
-    except subprocess.CalledProcessError as e:
-        print(f"Failed to clone repository: {e}")
+    else:
         return False
 
 
-def store_vector_embeddings(dataset_path, github_url, suffixes=[".py"]):
+def dataset_exists(dataset_name, store_locally):
+    if store_locally:
+        return dataset_exists_locally(dataset_name)
+    else:
+        return dataset_exists_online(dataset_name)
+
+
+def store_vector_embeddings(dataset_name, github_url, suffixes=[".py"], store_locally=False):
     with tempfile.TemporaryDirectory() as root_dir:
         if clone_github_repo(github_url, root_dir):
             # Load files
@@ -52,25 +58,33 @@ def store_vector_embeddings(dataset_path, github_url, suffixes=[".py"]):
                                                                            chunk_overlap=200)
             texts = python_splitter.split_documents(docs)
             # Make dataset
-            db = DeepLake(dataset_path=dataset_path, embedding=EMBEDDINGS)
+            if store_locally:
+                db = DeepLake(dataset_path=f"vector_store/{dataset_name}", embedding=EMBEDDINGS)
+            else:
+                db = DeepLake(dataset_path=f"hub://{deeplake_username}/{dataset_name}", embedding=EMBEDDINGS)
             db.add_documents(texts)
             return db
 
 
-class HelperBot:
-    REPO_NAME = None  # These will be overridden in subclasses
-    REPO_URL = None
-    SUFFIXES = None
+def clone_github_repo(github_url, local_path):
+    try:
+        subprocess.run(['git', 'clone', github_url, local_path], check=True)
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to clone repository: {e}")
+        return False
 
-    def __init__(self, package_name, github_url, suffixes=[".py"]):
-        self.package_name = package_name
-        self.dataset_path = f"hub://sapols/{package_name}"
-        if dataset_exists(self.dataset_path):
-            self.repo_ds = DeepLake(dataset_path=self.dataset_path, read_only=True, embedding=EMBEDDINGS)
+
+class HelperBot:
+    def __init__(self, package_name, github_url, suffixes=[".py"], use_local_vector_store=True):
+        if use_local_vector_store:
+            dataset_path = f"vector_store/{package_name}"
         else:
-            print(f"Creating a new vector store at: {self.dataset_path} ...")
-            self.repo_ds = store_vector_embeddings(self.dataset_path, github_url, suffixes)
-            print(f"Created a new vector store at: {self.dataset_path}")
+            dataset_path = f"hub://{deeplake_username}/{package_name}"
+        if not dataset_exists(package_name, use_local_vector_store):
+            # store it first
+            store_vector_embeddings(package_name, github_url, suffixes, use_local_vector_store)
+        self.repo_ds = DeepLake(dataset_path=dataset_path, read_only=True, embedding=EMBEDDINGS)
 
     def get_qa_chain(self, distance_metric='cos', fetch_k=100, maximal_marginal_relevance=True, k=10):
         retriever = self.repo_ds.as_retriever()
@@ -78,5 +92,5 @@ class HelperBot:
         retriever.search_kwargs['fetch_k'] = fetch_k
         retriever.search_kwargs['maximal_marginal_relevance'] = maximal_marginal_relevance
         retriever.search_kwargs['k'] = k
-        qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model=model_name), retriever=retriever)  # TODO: this is a little too strict; doesn't use its own knowledge when the answers aren't in the retrieved docs. E.g. we'll get "I'm sorry, but the provided context does not include information about HAPI and its data downloading process. Therefore, I can't provide a comparison between the data downloading processes of HAPI and pysat."
+        qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model=model_name), retriever=retriever)
         return qa
--- bot/pyhc_bots.py
+++ bot/pyhc_bots.py
@@ -6,61 +6,61 @@ class HapiBot(HelperBot):
     REPO_NAME = "hapiclient"
     REPO_URL = "https://github.com/hapi-server/client-python.git"
     SUFFIXES = [".py", ".md"]
-    def __init__(self):
-        super().__init__(HapiBot.REPO_NAME, HapiBot.REPO_URL, HapiBot.SUFFIXES)
+    def __init__(self, use_local_vector_store=True):
+        super().__init__(HapiBot.REPO_NAME, HapiBot.REPO_URL, HapiBot.SUFFIXES, use_local_vector_store)
 
 
 class KamodoBot(HelperBot):
     REPO_NAME = "kamodo"
     REPO_URL = "https://github.com/nasa/Kamodo.git"
     SUFFIXES = [".py", ".md"]
-    def __init__(self):
-        super().__init__(KamodoBot.REPO_NAME, KamodoBot.REPO_URL, KamodoBot.SUFFIXES)
+    def __init__(self, use_local_vector_store=True):
+        super().__init__(KamodoBot.REPO_NAME, KamodoBot.REPO_URL, KamodoBot.SUFFIXES, use_local_vector_store)
 
 
 class PlasmapyBot(HelperBot):
     REPO_NAME = "plasmapy"
     REPO_URL = "https://github.com/PlasmaPy/PlasmaPy.git"
     SUFFIXES = [".py", ".md"]
-    def __init__(self):
-        super().__init__(PlasmapyBot.REPO_NAME, PlasmapyBot.REPO_URL, PlasmapyBot.SUFFIXES)
+    def __init__(self, use_local_vector_store=True):
+        super().__init__(PlasmapyBot.REPO_NAME, PlasmapyBot.REPO_URL, PlasmapyBot.SUFFIXES, use_local_vector_store)
 
 
 class PysatBot(HelperBot):
     REPO_NAME = "pysat"
     REPO_URL = "https://github.com/pysat/pysat.git"
     SUFFIXES = [".py", ".rst"]  # TODO: include README.md but not other .md files, somehow?
-    def __init__(self):
-        super().__init__(PysatBot.REPO_NAME, PysatBot.REPO_URL, PysatBot.SUFFIXES)
+    def __init__(self, use_local_vector_store=True):
+        super().__init__(PysatBot.REPO_NAME, PysatBot.REPO_URL, PysatBot.SUFFIXES, use_local_vector_store)
 
 
 class PyspedasBot(HelperBot):
     REPO_NAME = "pyspedas"
     REPO_URL = "https://github.com/spedas/pyspedas.git"
     SUFFIXES = [".py", ".md"]
-    def __init__(self):
-        super().__init__(PyspedasBot.REPO_NAME, PyspedasBot.REPO_URL, PyspedasBot.SUFFIXES)
+    def __init__(self, use_local_vector_store=True):
+        super().__init__(PyspedasBot.REPO_NAME, PyspedasBot.REPO_URL, PyspedasBot.SUFFIXES, use_local_vector_store)
 
 
 class SpacepyBot(HelperBot):
     REPO_NAME = "spacepy"
     REPO_URL = "https://github.com/spacepy/spacepy.git"
     SUFFIXES = [".py", ".md"]
-    def __init__(self):
-        super().__init__(SpacepyBot.REPO_NAME, SpacepyBot.REPO_URL, SpacepyBot.SUFFIXES)
+    def __init__(self, use_local_vector_store=True):
+        super().__init__(SpacepyBot.REPO_NAME, SpacepyBot.REPO_URL, SpacepyBot.SUFFIXES, use_local_vector_store)
 
 
 class SunpyBot(HelperBot):
     REPO_NAME = "sunpy"
     REPO_URL = "https://github.com/sunpy/sunpy.git"
     SUFFIXES = [".py", ".rst"]
-    def __init__(self):
-        super().__init__(SunpyBot.REPO_NAME, SunpyBot.REPO_URL, SunpyBot.SUFFIXES)
+    def __init__(self, use_local_vector_store=True):
+        super().__init__(SunpyBot.REPO_NAME, SunpyBot.REPO_URL, SunpyBot.SUFFIXES, use_local_vector_store)
 
 
 class PyhcBot(HelperBot):
     REPO_NAME = "pyhc"
     REPO_URL = "https://github.com/heliophysicsPy/heliophysicsPy.github.io.git"
     SUFFIXES = [".md", ".yml"]
-    def __init__(self):
-        super().__init__(PyhcBot.REPO_NAME, PyhcBot.REPO_URL, PyhcBot.SUFFIXES)
+    def __init__(self, use_local_vector_store=True):
+        super().__init__(PyhcBot.REPO_NAME, PyhcBot.REPO_URL, PyhcBot.SUFFIXES, use_local_vector_store)
